ac0161db3d439841175f031d11c023ffa025a235
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-07 20:41:01 +0330

    add alarm mp3 file

--- alarm.py
+++ alarm.py
@@ -1,8 +1,15 @@
 from datetime import datetime
 from playsound import playsound
-alarm_time = input("Enter a time for alarm with this format (HH:MM:SS)")
+alarm_time = input("Enter a time for alarm with this format (HH:MM)")
 alarm_hour = alarm_time[0:2]
-alarm_second = alarm_time[3:5]
-alarm_miute = alarm_time[6:8]
+alarm_minute = alarm_time[3:5]
 while True:
     time_now = datetime.now()
+    c_hour = time_now.strftime("%H")
+    c_minute = time_now.strftime("%M")
+
+    if alarm_hour == c_hour:
+        if alarm_minute == c_minute:
+                print("Alarm!!!!!!!!!!!")
+                playsound("alarm.mp3")
+                break
