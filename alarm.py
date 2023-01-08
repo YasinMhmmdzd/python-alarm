dccdf0266a6be334df64d8489414b26d3e7d9d86
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-08 12:45:37 +0330

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -3,6 +3,7 @@ from playsound import playsound
 alarm_time = input("Enter a time for alarm with this format (HH:MM)")
 alarm_hour = alarm_time[0:2]
 alarm_minute = alarm_time[3:5]
+alarm_title = input("set your alarm title >> ")
 while True:
     time_now = datetime.now()
     c_hour = time_now.strftime("%H")
@@ -11,5 +12,6 @@ while True:
     if alarm_hour == c_hour:
         if alarm_minute == c_minute:
                 print("Alarm!!!!!!!!!!!")
+                print(alarm_title)
                 playsound("alarm.mp3")
                 break
